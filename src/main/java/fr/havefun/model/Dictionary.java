3095a435029f6cde22b8c0b06c945ccc1da90889
bliou <jacques.eid@gmail.com>
2016-06-01 21:23:24 +0200

    Init Project

---
+++ src/main/java/fr/havefun/model/Dictionary.java
@@ -0,0 +1,14 @@
+package fr.havefun.model;
+
+public class Dictionary {
+	private int id;
+	private String word;
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public void setWord(String word) {
+		this.word = word;
+	}
+}
